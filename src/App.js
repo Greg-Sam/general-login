998d7f13e103004a6384b886e671ba9ff644b286
Greg Samuelson <grssam@gmail.com>
2021-08-18 16:59:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Facebook from './components/Facebook';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>General Login</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          To get started, chose a login option
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Facebook />
       </header>
     </div>
   );
